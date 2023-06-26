b8ccbd0788df3642d003474ec9c8e1997055299b
Weiberle17 <maxweiberle63@gmail.com>
2023-06-26 20:18:27 +0200

    feat: Better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::process;
+
 use rigit::{run_requested_command, Args};
 
 fn main() {
-  let args: Args = Args::parse_args();
+  let args: Args = Args::parse_args().unwrap_or_else(|err| {
+    eprintln!("Problem parsing arguments: {err}");
+    process::exit(1);
+  });
 
-  run_requested_command(&args);
+  if let Err(e) = run_requested_command(&args) {
+    eprintln!("Application error: {e}");
+    process::exit(1);
+  };
 }
