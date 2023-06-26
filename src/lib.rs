4099c3859a0262176160e02fc08967fc77d485da
Weiberle17 <maxweiberle63@gmail.com>
2023-06-26 20:19:48 +0200

    feat: First setup for pull command

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod status;
 
 use status::StatusParentDir;
 use std::env;
+use pull::PullParentDir;
 
 pub struct Args {
   pub command: String,
@@ -10,21 +11,37 @@ pub struct Args {
 }
 
 impl Args {
-  pub fn parse_args() -> Args {
+  pub fn parse_args() -> Result<Args, String> {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() != 3 {
+      return Err("Usage: rigit [command] [directory]".to_string());
+    }
     let command = args[1].to_owned();
     let path = args[2].to_owned();
 
-    Args { command, path }
+    Ok(Args { command, path })
   }
 }
 
-pub fn run_requested_command(args: &Args) {
-  if args.command == "status" {
-    let parent_dir = StatusParentDir::build(&args.path);
-    parent_dir.printing();
-  } else {
-    panic!("Usable commands are: 'status', you used {}", args.command);
+pub fn run_requested_command(args: &Args) -> Result<(), String> {
+  match args.command.as_str() {
+    "status" => {
+      let parent_dir = StatusParentDir::build(&args.path);
+      parent_dir.printing();
+    }
+    "pull" => {
+      let parent_dir = PullParentDir::build(&args.path);
+      parent_dir.printing();
+    }
+    _ => {
+      let error = format!(
+        "Usable commands are: [status, pull], you used '{}'",
+        args.command
+      );
+      return Err(error);
+    }
   }
+  
+  Ok(())
 }
---
+++ src/pull.rs
@@ -0,0 +1,9 @@
+pub struct PullParentDir {
+  
+}
+
+impl PullParentDir {
+    pub fn build(arg: &str) -> PullParentDir {
+      
+    }
+}
