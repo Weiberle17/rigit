1338bfeb955a1e324466a64df5874ed94b2f1c34
Weiberle17 <maxweiberle63@gmail.com>
2023-06-27 07:40:30 +0200

    feat: Added version flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod pull;
 pub mod status;
 
-use status::StatusParentDir;
-use std::env;
 use pull::PullParentDir;
+use status::StatusParentDir;
+use std::{env, process};
 
 pub struct Args {
   pub command: String,
@@ -18,9 +18,14 @@ pub struct Dir {
 
 impl Args {
   pub fn parse_args() -> Result<Args, String> {
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
+    if args[1] == "--version" {
+      println!("Rigit version: {VERSION}");
+      process::exit(0);
+    } else if args.len() != 3 {
       return Err("Usage: rigit [command] [directory]".to_string());
     }
     let command = args[1].to_owned();
@@ -48,6 +53,6 @@ pub fn run_requested_command(args: &Args) -> Result<(), String> {
       return Err(error);
     }
   }
-  
+
   Ok(())
 }
