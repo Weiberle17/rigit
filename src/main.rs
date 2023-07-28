28edca6941b718fa2d34269a276a9410f3efe81f
Weiberle17 <maxweiberle63@gmail.com>
2023-07-28 09:48:22 +0200

    fix: Proper matching for subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 mod cli;
-mod status;
 mod repos;
+mod status;
 
 use clap::Parser;
-use cli::Cli;
+use cli::{Cli, Command};
 use repos::Repos;
 use status::run_status;
 
 fn main() {
   let args = Cli::parse();
-  let repos = Repos::get_repos(args.path).unwrap();
-  
-  match args.command.as_str() {
-    "status" => run_status(repos),
-    _ => eprintln!("Not a supported command"),
+
+  match args.command {
+    Command::Status { path, verbose } => {
+      let repos = Repos::get_repos(path).unwrap();
+      run_status(repos, verbose);
+    }
   }
 }
