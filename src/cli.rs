97d39dbaa76c7ae676dc9c8abaa0b53f9d987877
Weiberle17 <maxweiberle63@gmail.com>
2023-07-28 09:45:58 +0200

    feat: Proper subcommand and verbose flag added

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,21 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-#[derive(Default, Parser, Debug)]
+#[derive(Parser, Debug)]
 #[command(name = "rigit")]
 #[command(author = "Weiberle17 <maxweiberle63@gmail.com>")]
 #[command(version)]
 #[command(about = "Cli-tool which lets you perform a git action on multiple repositories", long_about = None)]
 pub struct Cli {
-  pub command: String,
-  pub path: String,
+  #[command(subcommand)]
+  pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+  /// Run git status on all repos in the following directory
+  Status {
+    path: String,
+    #[clap(short, long)]
+    verbose: bool,
+  },
 }
